b4e96f0c13679578d5bd189a0c4d2ef3bad1f754
AlexFromXD <cj06528836@gmail.com>
2020-03-24 14:27:01 +0800

    change date format && support conversion of date string to unix timestamp (#3)

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,45 @@
-import { commands, window, ExtensionContext, TextEdit, Position } from 'vscode';
+import { commands, window, ExtensionContext } from 'vscode'
 
-export function activate(context: ExtensionContext) {
+export function activate (context: ExtensionContext) {
 
-	const disposable = commands.registerCommand('extension.showTime', () => {
+  const disposable = commands.registerCommand('extension.showTime', () => {
 
-		const currentEditor = window.activeTextEditor;
-		TextEdit.insert(new Position(0, 0), 'f02h0h20hf0203hf082');
+    const currentEditor = window.activeTextEditor
+    if (!currentEditor) return
 
-		const focus = currentEditor?.selection;
-		if (focus && focus.isSingleLine) {
-			const document = currentEditor?.document;
-			const text = document?.lineAt(focus.start.line).text;
-			const timestamp = text?.match(/\d{10}/g);
-			if (timestamp) {
-				const date = new Date(Number(timestamp[0])*1000);
-				window.showInformationMessage(`${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`);
-			}
-		}
-	});
+    const focus = currentEditor.selection
+    if (!focus || !focus.isSingleLine) return
 
-	context.subscriptions.push(disposable);
+    const document = currentEditor.document
+    if (!document) return
+
+    const focusLine = document.lineAt(focus.start.line).text
+    if (!focusLine) return
+
+    const unixtime = focusLine.match(/\d{10,13}/g)
+    if (unixtime) {
+      let date: Date|undefined = undefined
+      if (unixtime[0].length === 10) {
+        date = new Date(Number(unixtime[0]) * 1000)
+      } else if (unixtime[0].length === 13) {
+        date = new Date(Number(unixtime[0]))
+      }
+
+      if (date) {
+        const dtf = new Intl.DateTimeFormat('en-ZA', {
+          weekday: 'long', hour12: false,
+          year: 'numeric', month: 'numeric', day: '2-digit',
+          hour: 'numeric', minute: 'numeric', second: 'numeric',
+        })
+        window.showInformationMessage(dtf.format(date))
+      }
+    }
+
+    const timestamp = Date.parse(focusLine)
+    if (timestamp) window.showInformationMessage(`timestamp in second: ${String(timestamp/1000)}`)
+  })
+
+  context.subscriptions.push(disposable)
 }
 
-export function deactivate() {}
+export function deactivate () {}
