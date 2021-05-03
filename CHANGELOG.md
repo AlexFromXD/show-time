35d3763b54e659774c4f5e84031f7f5409082f1e
Alex <alex.wu@moneybook.com.tw>
2021-05-03 17:34:59 +0800

    fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,33 +2,13 @@
 
 All notable changes to the "showtime" extension will be documented in this file.
 
-## [0.0.5] - 2021-05-02
-
-Fixed
-
-- Fix bug of multiple timestamp in the same line
-
-
-## [0.0.4] - 2020-07-01
-
-Changed
-
-- Use hover message to present convert result, so we don't need trigger anymore
-
-## [0.0.3] - 2020-06-26
+## [0.0.2] - 2021-05-03
 
 Added
 
-- Adds icon
-- Adds unit test
-- Show current datetime and timestamp by parsing `keyword: now`
+- Supports multiple conversion in single line
+- Add demo.gif in README.md
 
-## [0.0.2] - 2020-03-24
-
-Added
-
-- Supports conversion of datetime to timestamp
-
-## [0.0.1] - 2020-02-17
+## [0.0.1] - 2021-05-03
 
 - Initial release
--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,14 @@
-function getDateString (d: Date): string {
-  const dtf = new Intl.DateTimeFormat('en-ZA', {
-    weekday: 'long', hour12: false,
-    year: 'numeric', month: 'numeric', day: '2-digit',
-    hour: 'numeric', minute: 'numeric', second: 'numeric',
-  })
-  return dtf.format(d)
+function getDateString (date: Date): string {
+  const weekMap: { [index: number]: string } = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday'
+  }
+  return `${weekMap[date.getUTCDay()]}, ${date.toISOString()}`
 }
 
 type ParseResult = {
@@ -14,35 +18,41 @@ type ParseResult = {
 }
 
 export function parseFocusLine (line: string): ParseResult[] {
-  const tsList = line.match(/\d{13}|\d{10}/g)
-  if (tsList) {
-    return tsList.map(ts => {
-      return {
-        origin: ts,
-        datetime: ts.length === 10
-          ? getDateString(new Date(Number(ts) * 1000))
-          : getDateString(new Date(Number(ts)))
-      }
-    })
-  }
+
+  const result: ParseResult[] = []
 
   const lineIncludeNow = line.match(/now/)
   if (lineIncludeNow) {
-    const now = Date.now()
-    return [{
+    const now = new Date(Date.now())
+
+    result.push({
       origin: 'now',
-      datetime: getDateString(new Date(now)),
-      timestamp: now / 1000
-    }]
+      datetime: getDateString(now),
+      timestamp: now.getTime() / 1000
+    })
   }
 
-  const timestamp = Date.parse(line)
-  if (timestamp) {
-    return [{
-      origin: line,
-      timestamp: timestamp / 1000
-    }]
+  const tsList = line.match(/\d{10}\.\d{1,3}|\d{13}|\d{10}/g)
+  if (tsList) {
+    result.push(...tsList.map(ts => {
+      return {
+        origin: ts,
+        datetime: ts.length === 13 && !ts.includes('.')
+          ? getDateString(new Date(Number(ts)))
+          : getDateString(new Date(Number(ts) * 1000))
+      }
+    }))
   }
 
-  return []
+  const dateStringList = line.match(/(\d{4}(-|\/)\d{1,2}(-|\/)\d{1,2}T\d{2}:\d{2}:\d{2}\.\d{3})|(\d{4}(-|\/)\d{1,2}(-|\/)\d{1,2}T\d{2}:\d{2}:\d{2})|(\d{4}(-|\/)\d{1,2}(-|\/)\d{1,2})/g)
+  if (dateStringList) {
+    result.push(...dateStringList.map(date => {
+      return {
+        origin: date,
+        timestamp: new Date(date).getTime() / 1000
+      }
+    }))
+  }
+
+  return result
 }
